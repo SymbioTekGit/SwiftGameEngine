9fe6b703d69ff525c0ce580275a5b684580eac5a
Symbiosis Technology <symbiotek2012@gmail.com>
2024-11-24 09:37:33 +0400

    Initial Commit

---
+++ SwiftGameEngine macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  SwiftGameEngine macOS
+//
+//  Created by Alvin Heib on 24/11/2024.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
